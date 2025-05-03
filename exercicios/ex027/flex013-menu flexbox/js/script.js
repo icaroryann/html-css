b56d7a040e12227da75e54ff44faae4c6e2447b6
Icaro Leonel <103190983+icaroryann@users.noreply.github.com>
2025-05-02 21:04:43 -0300

    terminei exercicio 13 de flexbox

--- exercicios/ex027/flex013-menu flexbox/js/script.js
+++ exercicios/ex027/flex013-menu flexbox/js/script.js
@@ -1,3 +1,11 @@
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'flex'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+
 function clickMenu() {
     if (itens.style.display == 'flex') {
         itens.style.display = 'none'
